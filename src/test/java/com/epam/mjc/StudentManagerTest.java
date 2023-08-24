5f719e5628f90250c7277479ba6bfebe3b20fd2f
ElenaGeorgievska <elena_science@yahoo.com>
2023-08-24 15:57:03 +0200

    stage1_module1_task2_custom_exception completed

--- src/test/java/com/epam/mjc/StudentManagerTest.java
+++ src/test/java/com/epam/mjc/StudentManagerTest.java
@@ -9,44 +9,48 @@ import org.junit.Test;
 
 
 public class StudentManagerTest {
-  StudentManager manager = new StudentManager();
+    StudentManager manager = new StudentManager();
 
-  @Test(expected = IllegalArgumentException.class)
-  public void findNotValid() throws IllegalArgumentException {
-    manager.find(1000);
-  }
-
-  @Test
-  public void findValidStudent() {
-    try {
-      assertNotNull(manager.find(1));
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
+    @Test(expected = IllegalArgumentException.class)
+    public void findNotValid() throws IllegalArgumentException {
+        manager.find(1000);
     }
-  }
 
-  @Test
-  public void testExceptionMessage() {
-    try {
-      assertNotNull(manager.find(1000));
-    } catch (IllegalArgumentException e) {
-      assertEquals("Could not find student with ID 1000", e.getMessage());
+    @Test
+    public void findValidStudent() {
+        try {
+            assertNotNull(manager.find(1));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  @Test
-  public void testIDsNotChangedV1() {
-    try {
-      assertNull("Student enum should be have only 10 values.", manager.find(11));
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
+    @Test
+    public void testExceptionMessage() {
+        try {
+            assertNotNull(manager.find(1000));
+        } catch (IllegalArgumentException e) {
+            assertEquals("Could not find student with ID 1000", e.getMessage());
+        }
     }
-  }
 
-  @Test
-  public void testIDsNotChangedV2() {
-    assertEquals(Student.ARTUR, manager.find(Arrays.stream(Student.values()).map(Student::getId).max(Long::compare).get()));
+    @Test
+    public void testIDsNotChangedV1() {
+        try {
+            assertNull("Student enum should be have only 10 values.", manager.find(11));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
 
-  }
+    @Test
+    public void testIDsNotChangedV2() {
+        try {
+            assertEquals(Student.ARTUR, manager.find(Arrays.stream(Student.values()).map(Student::getId).max(Long::compare).get()));
+        } catch (StudentNotFoundException ex) {
+            System.err.print(ex);
+        }
+    }
+
 
 }
